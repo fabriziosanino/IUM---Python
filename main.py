2f374b91b7dcabb9361b49981ca4d5033a98d477
Nicole <nicole.gazzera@di.unito.it>
2021-11-03 19:58:03 +0100

    created new table with checkbox created function to memorize the matrix

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
-# Press the green button in the gutter to run the script.
-from searchPattern import *
 from tkinter import *
-root = Tk()
-v = IntVar()
-Radiobutton(root, text='GfG', variable=v, value=1).pack(anchor=W)
-Radiobutton(root, text='MIT', variable=v, value=2).pack(anchor=W)
-mainloop()
-
+from searchPattern import *
 
 if __name__ == '__main__':
     matrix = createMatrix()
-    printMatrix(pattern)
+
+window = Tk()
+window.title("IUM find Patterns")
+
+M = 4
+N = 4
+
+boxes = []
+boxVars = []
+
+# Create all IntVars, set to 0
+
+for i in range(M):
+    boxVars.append([])
+    for j in range(N):
+        boxVars[i].append(IntVar())
+        boxVars[i][j].set(0)
+
+def checkRow(i):
+    global boxVars, boxes
+    row = boxVars[i]
+    deselected = []
+
+    for j in range(len(row)):
+        if row[j].get() == 0:
+            deselected.append(j)
+
+def printMat():
+    for i in range(len(boxVars)):
+        temp = []
+        for j in range(len(boxVars[i])):
+                matrix[i][j]= boxVars[i][j].get()
+    printMatrix(matrix)
+
+
+for x in range(M):
+    boxes.append([])
+    for y in range(N):
+        boxes[x].append(Checkbutton(window, variable = boxVars[x][y], command = lambda x = x: checkRow(x)))
+        boxes[x][y].grid(row=x+1, column=y+1)
+
+b = Button(window, text = "Find patterns", command = printMat, width = 10)
+b.grid(row = 12)
+mainloop()
+
