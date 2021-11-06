50c501a0c4db62239f0dc74f495ae604085dff28
fabriziosanino <48118048+fabriziosanino@users.noreply.github.com>
2021-11-06 15:09:32 +0100

    Add function to print colored matrix

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ boxVars = []
 # Create all IntVars, set to 0
 
 if __name__ == '__main__':
-    matrixToFindIn = createMatrix(3, 3)
-    printMatrix(matrixToFindIn, 3, 3)
+    matrixToFindIn = createMatrix(30, 30)
+    printMatrix(matrixToFindIn)
 
     patternMatrix = createMatrixEmpty(rows, columns)
 
@@ -26,6 +26,7 @@ for i in range(rows):
         boxVars[i].append(IntVar())
         boxVars[i][j].set(0)
 
+
 def checkRow(i):
     global boxVars, boxes
     row = boxVars[i]
@@ -35,23 +36,22 @@ def checkRow(i):
         if row[j].get() == 0:
             deselected.append(j)
 
+
 def printMat():
     for i in range(len(boxVars)):
         temp = []
         for j in range(len(boxVars[i])):
-                patternMatrix[i][j]= boxVars[i][j].get()
-    print("Matrice Pattern: ")
-    printMatrix(patternMatrix, rows, columns)
+            patternMatrix[i][j] = boxVars[i][j].get()
+
     searchPattern(patternMatrix, matrixToFindIn)
 
 
 for x in range(rows):
     boxes.append([])
     for y in range(columns):
-        boxes[x].append(Checkbutton(window, variable = boxVars[x][y], command = lambda x = x: checkRow(x)))
-        boxes[x][y].grid(row=x+1, column=y+1)
+        boxes[x].append(Checkbutton(window, variable=boxVars[x][y], command=lambda x=x: checkRow(x)))
+        boxes[x][y].grid(row=x + 1, column=y + 1)
 
-b = Button(window, text = "Find pattern", command = printMat, width = 10)
-b.grid(row = 12)
+b = Button(window, text="Find pattern", command=printMat, width=10)
+b.grid(row=12)
 mainloop()
-
--- searchPattern.py
+++ searchPattern.py
@@ -1,5 +1,6 @@
 from random import seed
 from random import randint
+from termcolor import colored
 
 pattern = [[0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0]]
 
@@ -14,11 +15,14 @@ def createMatrixEmpty(rows, columns):
     return [[0 for x in range(columns)] for y in range(rows)]
 
 
-def printMatrix(matrix, rows, columns):
-    for row in range(0, rows):
+def printMatrix(matrix):
+    for row in range(0, len(matrix)):
         val = ""
-        for column in range(0, columns):
-            val += str(matrix[row][column]) + " "
+        for column in range(0, len(matrix[0])):
+            if matrix[row][column] == 5:
+                val += colored(str(matrix[row][column]), "green") + " "
+            else:
+                val += str(matrix[row][column]) + " "
         print(val)
 
 
@@ -27,14 +31,14 @@ def truncatePattern(matrix):
     minM = minN = float('inf')
     for row in range(0, len(matrix)):
         for column in range(0, len(matrix[row])):
-            if (matrix[row][column] == 1):
-                if (column < minN):
+            if matrix[row][column] == 1:
+                if column < minN:
                     minN = column
-                if(column > maxN):
+                if column > maxN:
                     maxN = column
-                if(row < minM):
+                if row < minM:
                     minM = row
-                if(row > maxM):
+                if row > maxM:
                     maxM = row
 
     return maxM, maxN, minM, minN
@@ -49,45 +53,52 @@ def createPattern(matrix, width, heigth, minM, minN):
 
     return mat
 
-def rotate_matrix(matrix): # senso anti orario di 90 gradi
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0])-1,-1,-1)]
+
+def rotateMatrix90Degree(matrix):  # senso anti orario di 90 gradi
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]) - 1, -1, -1)]
     #                                    |                           |
     #                                    ----------------------------->matrix[0] in quando l'altezza diventa come la grandezza e viceversa.
 
-def searchPattern(patternMatrixToTruncate, matrixToFindIn):
-    maxM, maxN, minM, minN = truncatePattern(patternMatrixToTruncate)
-    #print("MIN e MAX", minM, maxM, minN, maxN)
 
-    width = (maxN - minN) + 1  # gli indici partono da 0
-    height = (maxM - minM) + 1
-
-    #print("WIDTH e HEIGHT", width, height)
-
-    patternMatrix = createPattern(patternMatrixToTruncate, width, height, minM, minN)
-
-    patternMatrix = rotate_matrix(patternMatrix)
-
-    #printMatrix(patternMatrix, height, width)
-
-    count = 0
+def findPattern(patternMatrix, matrixToFindIn, height, width):
     for row in range(0, len(matrixToFindIn) + 1 - height):
         for column in range(0, len(matrixToFindIn[row]) + 1 - width):
             allTrue = False
             for i in range(0, height):
                 for j in range(0, width):
-                    if(matrixToFindIn[row + i][column + j] == patternMatrix[i][j]):
+                    if ((matrixToFindIn[row + i][column + j] == patternMatrix[i][j]) or (
+                            matrixToFindIn[row + i][column + j] == 5 and patternMatrix[i][j] == 1)):
                         allTrue = True
                     else:
                         allTrue = False
                         break
-                if(allTrue == False):
+                if (allTrue == False):
                     break
 
-            if(allTrue):
-                count += 1
+            if allTrue:
+                for i in range(0, height):
+                    for j in range(0, width):
+                        if matrixToFindIn[row + i][column + j] == 1:
+                            matrixToFindIn[row + i][column + j] = 5
 
 
-    print("")
-    print("Corrispondenze: ", count)
-    print("")
-    printMatrix(matrixToFindIn, len(matrixToFindIn), len(matrixToFindIn[0]))
+def searchPattern(patternMatrixToTruncate, matrixToFindIn):
+    maxM, maxN, minM, minN = truncatePattern(patternMatrixToTruncate)
+
+    width = (maxN - minN) + 1  # gli indici partono da 0
+    height = (maxM - minM) + 1
+
+    patternMatrix = createPattern(patternMatrixToTruncate, width, height, minM, minN)
+
+    findPattern(patternMatrix, matrixToFindIn, height, width)
+
+    patternMatrix = rotateMatrix90Degree(patternMatrix)
+    findPattern(patternMatrix, matrixToFindIn, width, height)
+
+    patternMatrix = rotateMatrix90Degree(patternMatrix)
+    findPattern(patternMatrix, matrixToFindIn, width, height)
+
+    patternMatrix = rotateMatrix90Degree(patternMatrix)
+    findPattern(patternMatrix, matrixToFindIn, width, height)
+
+    printMatrix(matrixToFindIn)
