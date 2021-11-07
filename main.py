038e1c27f7431f2685eb4effd7e6dedf98fb55d2
fabriziosanino <48118048+fabriziosanino@users.noreply.github.com>
2021-11-07 18:26:17 +0100

    Update graphics

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from searchPattern import *
 
 rows = 4
 columns = 4
+startViewMatrix = 2
 
 def printMat():
     for i in range(len(boxVars)):
         for j in range(len(boxVars[i])):
             patternMatrix[i][j] = boxVars[i][j].get()
 
+    #TODO: controllare che ci sia almeno un checkbox attivato!
+
     searchPattern(patternMatrix, matrixToFindIn)
+
 
 def checkRow(i):
     global boxVars, boxes
@@ -23,7 +27,9 @@ def checkRow(i):
 if __name__ == '__main__':
     window = Tk()
     window.title("IUM find Patterns")
-    b = Button(window, text = "Find pattern", command = printMat, width=10)
+    window.geometry("900x700+300+50")
+    labelStart = Label(window, text = "Inserisci il pattern da cercare", font=("Arial", 20))
+    buttonFindPattern = Button(window, text = "Find pattern", command = printMat, width=10)
 
     boxes = []
     boxVars = []
@@ -38,9 +44,10 @@ if __name__ == '__main__':
         boxes.append([])
         for y in range(columns):
             boxes[x].append(Checkbutton(window, variable=boxVars[x][y], command=lambda x=x: checkRow(x)))
-            boxes[x][y].grid(row=x + 1, column=y + 1)
+            boxes[x][y].grid(row=x + 1 + startViewMatrix, column=y + 10)
 
-    b.grid(row=12)
+    buttonFindPattern.grid(row=40)
+    labelStart.grid(row=1, column = 400)
 
     matrixToFindIn = createMatrix(5, 5)
 
